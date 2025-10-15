e3b87d46b8ceccd44efc03e7bd1e17e0e2cf2f30
abdulbasit <proabdulbasit.me@gmail.com>
2025-10-16 00:25:39 +0500

    initial website setup

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="flex h-16 w-full items-center justify-center bg-white shadow-md">
+      Header
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+import HomePage from './features/HomePage/homePage';
+
+export default function Home() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
